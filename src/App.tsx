06ee1a01bb55b702bece472ddb1ab2a2e3e55c2c
JuliaKardelice <julia.royer@live.fr>
2025-05-04 12:55:01 +0200

    correction emplacement de composants

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.scss";
 
-import { Footer } from "./src/common/footer/Footer";
-import { Header } from "./src/common/header/Header";
-import { Contact } from "./src/pages/contact/Contact";
-import { Home } from "./src/pages/home/Home";
+import { Footer } from "./common/footer/Footer";
+import { Header } from "./common/header/Header";
+import { Contact } from "./pages/contact/Contact";
+import { Home } from "./pages/home/Home";
 
 export const App: React.FC = () => {
   return (
